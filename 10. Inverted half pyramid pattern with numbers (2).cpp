2460a5309feabf3236ecf51f6b8524ca603b6fc5
Shrabani Singha <137190575+shb-sn@users.noreply.github.com>
2023-07-02 14:37:52 +0530

    Add files via upload

---
+++ 10. Inverted half pyramid pattern with numbers (2).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for (int j=1; j<=n-i+1; j++){
+            if (j<=n-i){
+                cout<<"  ";
+            }
+            else{
+                cout<<j<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+return 0;
+}
